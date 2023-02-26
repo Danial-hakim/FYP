408e43927b42e397527d1feb2ca577fe2a20583d
Danial <danialhakim01@gmail.com>
2023-02-26 13:37:57 +0000

    Weapon in progress

--- LastProject/Assets/Scripts/MagniferMover.cs
+++ LastProject/Assets/Scripts/MagniferMover.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class MagniferMover : MonoBehaviour
 {
     public float speed = 10.0f;
+    public Transform playerCamera;
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+        // Get the camera's local right, up, and forward vectors
+        Vector3 right = playerCamera.right;
+        Vector3 up = playerCamera.up;
+        Vector3 forward = playerCamera.forward;
+
+        // Project the forward vector onto the XZ plane
+        forward.y = 0;
+
+        // Normalize the vectors
+        right.Normalize();
+        up.Normalize();
+        forward.Normalize();
+
+        // Calculate the movement vector based on the input and camera orientation
+        Vector3 movement = (horizontal * right + vertical * forward) * speed * Time.deltaTime;
+
+        // Move the magnifier along with the camera
+        transform.position += movement;
     }
 }
